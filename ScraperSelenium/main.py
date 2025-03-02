f1383856633f6aea216928d940b1cfd55094b008
mlk85 <malaczek85@interia.pl>
2025-03-02 21:04:44 +0100

    main included

--- ScraperSelenium/main.py
+++ ScraperSelenium/main.py
@@ -1,93 +1,89 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
+import json
 import time
-import os
 
-def create_driver(executable_path="chromedriver.exe"):
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")  # Uruchamia w tle (opcjonalne)
-    service = Service("chromedriver.exe")  # Podaj ścieżkę do chromedriver.exe
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    return driver
+import requests
+from bs4 import BeautifulSoup
 
-
-def scroll_page(driver, max_scrolls=140, sleep_time=2):
+def scrape_data(query):
     """
-    Przewija stronę w dół do momentu załadowania nowej treści
-    (lub do osiągnięcia liczby max_scrolls).
+    Sends requests to the specified url and extracts data from the response
+    until there is no more content available on the page
+    Args:
+        query: The name of category to search
     """
-    scroll_attempts = 0
-    last_height = driver.execute_script("return document.body.scrollHeight")
-
-    while scroll_attempts < max_scrolls:
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(sleep_time)
-
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == last_height:
-            print("No more content to load. Exiting scroll loop.")
+    page = 1
+    all_publications = []
+    start = time.time()
+    print(f"Scraping data from the '{topic}' website...")
+    while True:
+        url = f"https://dblp.org/search/publ/inc?q={query.replace(' ', '%20')}%3A2022&s=ydvspc&h=30&b={page}"
+        response = requests.get(url=url)
+        if response.status_code == 200:
+            response_html_data = response.text
+            soup = BeautifulSoup(response_html_data, 'html.parser')
+            current_page_publications = search_for_publications(soup)
+            if not current_page_publications:
+                print(f"End of the '{topic}' website. Pages iterated: {page}")
+                break
+            all_publications.extend(current_page_publications)
+            page += 1
+        else:
+            print(f"Error: {response.status_code}")
             break
+    finish = time.time()
+    print(f"Scraping the '{topic}' page took {finish - start:.5}s.")
+    return all_publications
 
-        last_height = new_height
-        scroll_attempts += 1
-        print(f"Scroll attempt {scroll_attempts}.")
-
-def scrape_dblp(query):
-    url = f"https://dblp.org/search?q={query}"
-    driver = create_driver()
-    driver.get(url)
-
-    scroll_page(driver)
-
-    page_source = driver.page_source
-    driver.quit()
-
-    # Parsowanie HTML
-    soup = BeautifulSoup(page_source, 'html.parser')
+def search_for_publications(soup):
+    """
+    Looks for publications data in provided html content
+    Args:
+        soup: bs4 object of html content
+    """
     publications = []
-
-    for entry in soup.find_all("li", class_="entry"):  # Szukanie wpisów
+    for entry in soup.find_all("li", class_="entry"):
         title_tag = entry.find("span", class_="title")
         authors = [a.text for a in entry.find_all("span", itemprop="author")]
         year_tag = entry.find("span", itemprop="datePublished")
 
         if title_tag and year_tag:
-            publications.append({
-                "title": title_tag.text,
-                "authors": authors,
-                "year": year_tag.text,
-            })
-
-    # Zapisz dane do pliku
-    #folder_name = query.split(":")[0] #Used for creating the folder for each of the topics
-    folder_path = os.path.join("Dane")  # Ścieżka do folderu
-    os.makedirs(folder_path, exist_ok=True)
-
-    filename = query.replace(":", "_") + ".txt"
-    file_path = os.path.join(folder_path, filename)
-    with open(file_path, "w", encoding="utf-8") as file:
-        for pub in publications:
-            file.write(f"Tytuł: {pub['title']}\n")
-            file.write(f"Autorzy: {', '.join(pub['authors'])}\n")
-            file.write(f"Rok: {pub['year']}\n")
-            file.write("\n")
-
-    print("Dane zapisane do pliku: ", file_path)
+            if title_tag.text != "(Withdrawn)": #skips empty tags
+                publications.append({
+                    "title": title_tag.text,
+                    "authors": authors,
+                    "year": year_tag.text,
+                })
     return publications
 
+def save_publications_to_file(publications, file_name):
+    """
+    Saves publications data to file with given name
+    Args:
+        publications: list of publications to save
+        file_name: name of file to save data to
+    """
+    start = time.time()
+    with open(f"scrapped_data/{file_name}.json", "w")as file:
+        json.dump(publications, file, indent=4)
+    finish = time.time()
+    print(f"Data saved to file 'scrapped_data/{file_name}'.")
+    print(f"Saving data to '{file_name}' file took {finish - start:.3f}s.\n\n")
+
+
 if __name__ == "__main__":
-    #topics = ["artificial intelligence", "distributed systems", "5G", "manet", 
-    #"swarm intelligence", "quantum computing", "graphs", "temporal networks", "deep learning"]
-    
-    topics = ["5G"]
+    topics = [
+        "artificial intelligence", "distributed systems", "5G", "manet","swarm intelligence",
+        "quantum computing", "graphs", "temporal networks", "deep learning"
+    ]
+
+    print("Program started.\n\n")
+    start_of_whole_program = time.time()
 
     for topic in topics:
-        for i in range(2022, 2023): #Change to 1980, 2024 for a full range
-            temp_query = f"{topic}:{i}"
-            print(temp_query)
-            results = scrape_dblp(temp_query)
+        pubs = scrape_data(topic)
+        save_publications_to_file(pubs, topic.replace(" ", "_").lower())
+
+    end_of_whole_program = time.time()
+    whole_time = (end_of_whole_program - start_of_whole_program)
+    print("Program finished.")
+    print(f"It took {whole_time:.5f}s to scrape and save all the data.")
